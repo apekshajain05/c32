56bf768a52701fbacd4270b686850f86a2bce29d
apekshajain05 <ashypreet@gmail.com>
2020-05-16 16:52:47 +0530

    abc

--- sketch.js
+++ sketch.js
@@ -7,11 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg="sprites/bg.png";
+var score=0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   // backgroundImg = loadImage("sprites/bg.png");
+   time();
 }
 
 function setup(){
@@ -45,18 +48,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
+    }
+    textSize(30);
+    fill("white");
+    text("Score "+score,900,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -66,7 +76,8 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display(); 
+   
 }
 
 function mouseDragged(){
@@ -85,4 +96,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+ async function time(){
+    
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJSON=await response.json();
+   //console.log(responseJSON);
+    var dt=responseJSON.datetime;
+  //  console.log(dt);
+  var hour=dt.slice(11,13);
+  //console.log(hour);
+    if(hour>=6&&hour<=19){
+        bg="sprites/bg.png";
+    }
+    else{
+        bg="sprites/bg2.jpg";
+    }
+    backgroundImg=loadImage(bg);
 }
